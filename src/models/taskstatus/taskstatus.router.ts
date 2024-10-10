8755bae8a3f96fb390f3bc2aa95bdc0471520f27
yesthis9090@gmail.com <yesthis9090@gmail.com>
2024-10-10 17:09:57 +0900

    :sparkles: feat: 업무 상태 관련 기능 추가

--- src/models/taskstatus/taskstatus.router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import TaskStautsController from './taskstatus.controller';
-
-const TaskStautsRouter = Router();
-
-//생성
-TaskStautsRouter.post('/', TaskStautsController.createTaskStauts);
-
-export default TaskStautsRouter;
--- src/models/taskstatus/taskstatus.schema.ts
+++ src/models/taskstatus/taskstatus.schema.ts
@@ -1,21 +1,22 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose, { Schema } from "mongoose";
 
 const taskStatusSchema = new Schema({
-  taskId: { type: mongoose.Schema.Types.ObjectId, ref: 'Task', required: true },
+  taskId: { type: mongoose.Schema.Types.ObjectId, ref: "Task", required: true },
   startDate: { type: Date, default: null },
   endDate: { type: Date, default: null },
   status: {
     type: String,
-    enum: ['할일', '진행중', '완료'],
+    enum: ["할일", "진행중", "완료"],
     default: null,
   },
   priority: {
     type: String,
-    enum: ['높음', '중간', '낮음'],
+    enum: ["높음", "중간", "낮음"],
     default: null,
   },
+  crew_member: [{ type: Schema.Types.ObjectId, ref: "User", required: true }],
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
 
-export default mongoose.model('Task_Status', taskStatusSchema);
+export default mongoose.model("Task_Status", taskStatusSchema);
